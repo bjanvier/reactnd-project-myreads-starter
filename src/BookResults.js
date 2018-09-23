b510c1cf9ccafa11bc97554f191b3ff6c257678d
janvierzagabe <bwemerezagabe@yahoo.fr>
2018-09-23 13:11:56 -0500

    I added proptypes

--- src/BookResults.js
+++ src/BookResults.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types'
 
 const shelfOptions = 
 [ 
@@ -96,5 +96,11 @@ allShelvesUpdated.push(
             </section>
      </li>
     )
+
 }
+BookResults.propTypes = {
+    movingBooks: PropTypes.func.isRequired,
+    book: PropTypes.object.isRequired,
+    books: PropTypes.array.isRequired,
+  }
 export default BookResults
